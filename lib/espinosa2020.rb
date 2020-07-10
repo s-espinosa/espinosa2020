b84f9edb45f5918c0d132fabac84320ed7ff33c6
Sal Espinosa <sespinos@gmail.com>
2020-07-09 21:55:38 -0600

    Initial commit

---
+++ lib/espinosa2020.rb
@@ -0,0 +1,6 @@
+require "espinosa2020/version"
+
+module Espinosa2020
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/espinosa2020_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Espinosa2020 do
+  it "has a version number" do
+    expect(Espinosa2020::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
